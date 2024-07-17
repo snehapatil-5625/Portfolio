28c3e95c4045a31983ab4df02029a01f17b44a01
sneha patil <sneha.patil@anthaathi.co>
2024-07-17 23:15:25 +0530

    feat: basic component design

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Client as Styletron } from "styletron-engine-monolithic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider, styled } from "baseui";
+import DefaultLayout from "./DefaultLayout/layout";
+
+const engine = new Styletron();
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <DefaultLayout />
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
 
-export default App
+export default App;
